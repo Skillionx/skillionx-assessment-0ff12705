1e31336ab7c390f741a97b2236c542add3d87149
Skillionx <noreply@skillionx.com>
2026-06-16 10:38:01 +0200

    Add starter code

---
+++ starter.cs
@@ -0,0 +1,11 @@
+Imports System
+
+Module Module1
+
+    Sub Main()
+        Console.WriteLine("Welkom bij de Klantenbeheer Applicatie!")
+        ' Hier komt de logica voor de applicatie
+        Console.ReadLine()
+    End Sub
+
+End Module
